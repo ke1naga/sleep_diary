afa121da7d029ec8479cd35aa394432b5e0869a1
ke1naga <ke1naga.1111@gmail.com>
2025-01-20 19:45:11 +0900

    分変換nullフィルタ

--- public/script.js
+++ public/script.js
@@ -176,12 +176,16 @@ function drawGraph(dates, values, values2,bedTimes,wakeUpTimes) {
 
 
     // 起床時間と就寝時間を分単位で変換
-    const bedTimesInMinutes = bedTimes.map(time => {
+    const bedTimesInMinutes = bedTimes
+    .filter(time => time != null && time.trim() !== '') // null または 空の文字列を除外map(time => {
+    .map(time => {
         const [hours, minutes] = time.split(':').map(Number);
         return hours * 60 + minutes; // 分単位に変換
     });
 
-    const wakeUpTimesInMinutes = wakeUpTimes.map(time => {
+    const wakeUpTimesInMinutes = wakeUpTimes
+    .filter(time => time != null && time.trim() !== '') // null または 空の文字列を除外
+    .map(time => {
         const [hours, minutes] = time.split(':').map(Number);
         return hours * 60 + minutes; // 分単位に変換
     });
