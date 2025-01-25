d3be4a4c8ffbf33dce84c80ad9e1bbb90000497a
ke1naga <ke1naga.1111@gmail.com>
2025-01-25 15:21:50 +0900

    connectTimeout

--- server.cjs
+++ server.cjs
@@ -13,7 +13,8 @@ const connection = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  connectTimeout: 30000, // 30秒で接続タイムアウト
 });
 
 // 非同期接続テスト
